ab52518adf4f8053a9f15bee8882d76af657ac88
wendiyu <36240636+wendiyu@users.noreply.github.com>
2019-02-14 20:46:46 -0500

    Update RollDie.java

--- RollDie.java
+++ RollDie.java
@@ -4,7 +4,6 @@
  * Created on: Feb 12 2019
  * Created for: ICS4U
  * This program randomly generate a value between 1 and maxValue 
- *  then place the value 
  *
  ****************************************************************************/
 import java.util.Scanner;
@@ -18,24 +17,24 @@ public class RollDie {
 		int maxValue;
 		Scanner userInput = new Scanner(System.in);
 		
-		System.out.print("Please, put a positive integer for your max value: ");
+		System.out.println("Please, put a positive integer for your max value: ");
 		maxValue = userInput.nextInt();
 		
 		RollDie(maxValue);
 				
 	}
 	
-	public static int RollDie(int maxValue) {
+	public static int RollDie (int maxValue) {
 		
 		// die does not have a negative number 
 		if(maxValue < 1) {
-			System.out.print("Please, put a positive number! Example : 1, 2, 3");
+			System.out.println("Please, put a positive integer number! Example : 1, 2, 3");
 			return -1;
 			
 		}
-		// number can not get lower than 1
+		// random number can not get lower than 1
 		else if(maxValue == 1) {
-			System.out.print("1");
+			System.out.println("1");
 			return 1;
 		}
 		// generating random number under the maxValue that user had selected
@@ -46,10 +45,12 @@ public class RollDie {
 			
 			randomNum = range.nextInt(maxValue) + 1;
 			
-			System.out.print("Your number is " + randomNum + ".");
+			System.out.println("Your random number is " + randomNum + ".");
+			
 			return randomNum;
 		}
 		
 	}
 
 }
+
